e920c0b6110b1e5b5513c5dbe1b4d351bfef6e09
Igor B <yohabera9051@gmail.com>
2022-11-13 19:42:34 +0300

    add task №3

--- dz_req.py
+++ dz_req.py
@@ -1,9 +1,24 @@
 import requests
 import time
 
+# Задача №1
+
+
 
 # Задача №3
-def get_questions():
-    base_host = ""
-    req_uri = ""
-    our_time = int(time.time()) - 48 * 36
+# def get_questions():
+#     base_host = "https://api.stackexchange.com/"
+#     req_uri = "questions/"
+#     our_time = int(time.time()) - 48 * 3600
+#     params = {
+#         "pagesize": 100,
+#         "tagged": "Python",
+#         "fromdate": our_time,
+#         "site": "stackoverflow"
+#     }
+#     response = requests.get(base_host + req_uri, params=params)
+#     for question in response.json()["items"]:
+#         print(question['title'])
+#
+#
+# get_questions()
